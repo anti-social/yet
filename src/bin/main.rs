abe327ccd15451b8b4075418b0cf72aaf62abeb8
Alexander Koval <kovalidis@gmail.com>
2019-05-31 16:55:08 +0300

    Rendering multiple templace at once

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,29 +3,33 @@ use std::env;
 use std::io::Write;
 use std::path::PathBuf;
 
-use argparse::{ArgumentParser, Store, StoreOption};
+use argparse::{ArgumentParser, Store, List, StoreOption};
 
 use quire::emit_ast;
+use quire::ast::Ast;
 
 use yet::template::{parse_template, parse_values, render};
 
 fn main() -> Result<(), failure::Error> {
-    let mut template_path = PathBuf::new();
+    let mut template_paths = Vec::<PathBuf>::new();
     let mut values_path = None::<PathBuf>;
 
     {
         let mut ap = ArgumentParser::new();
         ap.set_description("Yaml template rendering tool");
-        ap.refer(&mut template_path)
+        ap.refer(&mut template_paths)
             .required()
-            .add_argument("template", Store, "Template to render");
+            .add_argument("templates", List, "Templates to render");
         ap.refer(&mut values_path)
             .add_option(&["--values"], StoreOption, "Values file");
 
         ap.parse_args_or_exit();
     }
 
-    let templates = parse_template(&template_path)?;
+    let mut templates = Vec::<Ast>::new();
+    for template_path in template_paths {
+        templates.extend(parse_template(&template_path)?);
+    }
     let values = match values_path {
         Some(p) => Some(parse_values(&p)?),
         None => None,
